c24d0f7519e3dee7aea53e9f1fd9e2853bab9abe
Nadia Garcia <nadia.garcia@tupad.utn.edu.ar>
2026-09-08 15:16:51 -0300

    Implementa catálogo dinámico con filtros y búsqueda

--- src/pages/client/home/home.ts
+++ src/pages/client/home/home.ts
@@ -1,5 +1,11 @@
 import "../../../main";
 import { logout } from "../../../utils/auth";
+// Importa el array con las categorias
+import { getCategories, PRODUCTS } from "../../../data/data";
+// impora para indicar el tipo de una categoria. 
+import type { ICategoria } from "../../../types/categoria";
+import{ agregarProductoAlCarrito } from "../../../utils/cart";
+
 
 const buttonLogout = document.getElementById(
   "logoutButton"
@@ -8,4 +14,98 @@ buttonLogout?.addEventListener("click", () => {
   logout();
 });
 
+// Obtiene los elementos necesarios del DOM
+const contenedorCategorias = document.getElementById("contenedorCategorias")!;
+const contenedorProductos = document.getElementById("contenedorProductos" )!;
+const buscador = document.getElementById("buscador" )as HTMLInputElement;
+const botonMostrarTodos = document.getElementById("mostrarTodos") as HTMLButtonElement;
+
+// Guarda la categoría seleccionada para aplicar el filtro
+// Creás categoriaActiva con let porque cambia al hacer clic. 
+// Puede guardar una ICategoria o null si todavía no hay ninguna seleccionada.
+let categoria_Activa : ICategoria | null= null;
+
+// vacía el contenedor para evitar duplicados.
+const dibujarCategorias = () : void =>{
+    contenedorCategorias.innerHTML = "";
+
+    // Guarda el array con todas las categorias disponibles
+    const categorias = getCategories();
+
+    // Obtiene las categorías y las recorre con forEach.
+    categorias.forEach((categoria) => {
+        const button = document.createElement('button');
+        button.textContent = categoria.nombre;
+
+        //compara esa categoría con la activa:
+        //si es la misma, le pone la clase activa;
+        // si no, le pone la clase normal.
+        button.className = 
+          categoria === categoria_Activa 
+          ? "btn-activo " 
+          : "categoria-btn";
+
+          // Guarda la categoria clickeada como activa y vuele a ejcutar la función 
+          // para que el boton seleccionado se va activo. 
+        button.addEventListener("click", () =>{
+          categoria_Activa = categoria;
+          dibujarCategorias();
+          dibujarProductos();
+        })
+
+        // Agrega el boton ya configurado dentro del contenedor
+        contenedorCategorias.appendChild(button);
+    });
+
+};
+
+
+const dibujarProductos = () : void => {
+  contenedorProductos.innerHTML = "";
+  
+  const productos = PRODUCTS.filter ((producto)=> {
+    const coincide_producto = 
+    categoria_Activa === null || 
+      producto.categorias.some((categoria)=> categoria.id === categoria_Activa?.id
+    );
+   
+    const coince_nombre = producto.nombre
+    .toLocaleLowerCase()
+    .includes(buscador.value.toLocaleLowerCase());
+
+    return coincide_producto && coince_nombre
+  });
+
+  productos.forEach((producto)=> {
+    const card = document.createElement("div");
+    card.className = "card-producto";
+    card.innerHTML = ` 
+        <img src="${producto.imagen}" alt="${producto.nombre}">
+        <h3>${producto.nombre}</h3>
+        <p>${producto.descripcion}</p>
+        <p>$${producto.precio} </p>
+        <button type="button" class="btn-agregar">Agregar</button>
+        `;
+    const botonAgregar = card.querySelector(".btn-agregar") as HTMLButtonElement;
+    botonAgregar.addEventListener("click", () =>{
+      agregarProductoAlCarrito(producto);
+    });
+    contenedorProductos.appendChild(card);  
+  });
+};
+
+buscador.addEventListener("input",()=>{
+  dibujarProductos();
+});
+
+botonMostrarTodos.addEventListener("click", () =>{
+  categoria_Activa = null;
+  dibujarCategorias();
+  dibujarProductos();
+})
+
+// Llamo a la función para que los botones aparezcan
+dibujarCategorias();
+
+dibujarProductos();
 
